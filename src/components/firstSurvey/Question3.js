0890095c78a914cf5ac37b2ba4d29017c469516a
Lucas <lucas.c.hurpia@gmail.com>
2023-06-18 02:41:13 -0700

    [feature] adds third question component

--- src/components/firstSurvey/Question3.js
+++ src/components/firstSurvey/Question3.js
@@ -1,48 +1,81 @@
-import React from 'react';
-import { View, Text, TouchableOpacity, FlatList, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Button } from 'react-native';
 
-const answerOptions = ['Option 1', 'Option 2', 'Option 3', 'Option 4'];
+const answerOptions = [
+  { text: 'Option 1', styles: { top: '22%', left: '2%' }, radius: 65 },
+  { text: 'Option 2', styles: { top: '10%', right: '0%' }, radius: 75 },
+  { text: 'Option 3', styles: { top: '45%', left: '5%' }, radius: 85 },
+  { text: 'Option 4', styles: { top: '37%', right: '-5%' }, radius: 55 },
+  { text: 'Option 5', styles: { bottom: '80%', left: '10%' }, radius: 55 },
+  { text: 'Option 6', styles: { bottom: '10%', right: '-20%' }, radius: 60 },
+  { text: 'Option 7', styles: { bottom: '22%', right: '8%' }, radius: 70 },
+];
 
-const Question3 = ({ onAnswer }) => {
-  const renderAnswerOption = ({ item }) => {
-    return (
-      <TouchableOpacity
-        style={styles.answerOption}
-        onPress={() => onAnswer(item)}
-      >
-        <Text>{item}</Text>
-      </TouchableOpacity>
-    );
+const Question3 = ({ onAnswer, onGoBack }) => {
+  const [selectedOptions, setSelectedOptions] = useState([]);
+
+  const handleAnswerOptionClick = (option) => {
+    setSelectedOptions((prevOptions) => {
+      if (prevOptions.includes(option.text)) {
+        // Option already selected, deselect it
+        return prevOptions.filter((prevOption) => prevOption !== option.text);
+      } else {
+        // Option not selected yet, add it
+        return [...prevOptions, option.text];
+      }
+    });
+  };
+
+  const handleSubmit = () => {
+    onAnswer(selectedOptions);
   };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.headerText}>
-        We'd like to know you better...
-      </Text>
-      <Text style={styles.questionText}>Do you have any diet type?</Text>
-      <FlatList
-        data={answerOptions}
-        numColumns={2}
-        renderItem={renderAnswerOption}
-        keyExtractor={(item, index) => index.toString()}
-        contentContainerStyle={styles.answerOptionsContainer}
-      />
+      <Text style={styles.questionText}>Tap items you like!</Text>
+
+      <View style={styles.answerOptionsContainer}>
+        {answerOptions.map((option) => {
+          const isSelected = selectedOptions.includes(option.text);
+          return (
+            <TouchableOpacity
+              key={option.text}
+              style={[
+                styles.answerOption,
+                { backgroundColor: isSelected ? 'gray' : 'white' },
+                option.styles,
+                { width: option.radius * 2, height: option.radius * 2, borderRadius: option.radius },
+              ]}
+              onPress={() => handleAnswerOptionClick(option)}
+            >
+              <Text>{option.text}</Text>
+            </TouchableOpacity>
+          );
+        })}
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <Button
+          title="<"
+          color="gray"
+          onPress={onGoBack}
+        />
+        <Button
+          title="Next Step"
+          color="gray"
+          onPress={handleSubmit}
+        />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  headerText: {
-    fontStyle: 'normal',
-    fontWeight: '700',
-    fontSize: 32,
-    textAlign: 'center',
-    marginBottom: 20,
+    padding: 12,
   },
   questionText: {
     fontStyle: 'normal',
@@ -52,16 +85,21 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   answerOptionsContainer: {
-    alignItems: 'center',
+    flex: 1,
+    width: '100%',
   },
   answerOption: {
-    width: 150,
-    height: 150,
-    backgroundColor: 'white',
-    borderRadius: 10,
-    margin: 10,
+    position: 'absolute',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 0,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    padding: 10,
   },
 });
 
