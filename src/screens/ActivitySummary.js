439a7d5318dbd249edef0469a9ed79e5fe8541a5
obikakelvin <obikakelvin.2255@gmail.com>
2023-07-06 17:35:16 -0700

    chore: adds style to screen

--- src/screens/ActivitySummary.js
+++ src/screens/ActivitySummary.js
@@ -1,5 +1,11 @@
 import React, { useContext, useEffect } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, ScrollView, Text, StyleSheet } from 'react-native';
+import { SvgXml } from 'react-native-svg';
+
+import Button from '../components/Button';
+import Spacer from '../components/Spacer';
+import Aligner from '../components/Aligner';
+import yay from '../components/assets/icons/yay';
 import SurveyContext from '../context/SurveyContext';
 
 const ActivitySummary = ({ navigation }) => {
@@ -12,17 +18,36 @@ const ActivitySummary = ({ navigation }) => {
   }, []);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>{surveyData.ActivityParameters.name}</Text>
-      <Text style={styles.subtitle}>You are close to setting your plan...</Text>
-      <Text style={styles.data}>{JSON.stringify(surveyData, null, 2)}</Text>
-      <Button
-        title="yay!"
-        color="gray"
-        style={styles.button}
-        onPress={() => navigation.navigate('SurveyResults')}
-      />
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+        <Text style={styles.activity}>{surveyData.ActivityParameters.name}</Text>
+        <Text style={styles.title}>Thank you</Text>
+        <Text style={styles.subtitle}>You are close to setting your plan...</Text>
+        
+        <View style={styles.yayCard}>
+            <Aligner direction="column">
+                <SvgXml xml={yay()} />
+                <Text style={styles.yayText}>Yay!!</Text>
+            </Aligner>
+        </View>
+
+        <Spacer type="margin" position="bottom" customSize={20} />
+
+        <Button
+            title="Choose Plan!"
+            color="gray"
+            style={styles.button}
+            onPress={() => navigation.navigate('SurveyResults')}
+        />
+
+        <Spacer type="margin" position="bottom" customSize={20} />
+
+        <Button
+            title="Back to Group"
+            color="gray"
+            style={styles.button}
+            onPress={() => navigation.navigate('SurveyResults')}
+        />
+    </ScrollView>
   );
 };
 
@@ -33,14 +58,18 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     padding: 12,
   },
-  title: {
+  activity: {
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 20,
+    textAlign: 'left'
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
   },
   subtitle: {
     fontSize: 20,
-    fontWeight: 'bold',
     marginBottom: 20,
   },
   data: {
@@ -53,7 +82,18 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     paddingVertical: 10,
   },
-  
+  yayCard: {
+    height: 400,
+    width: 305,
+    backgroundColor: '#0BC9B9',
+    borderRadius: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  yayText: {
+    fontSize: 40,
+    color: 'white'
+  }
 });
 
 export default ActivitySummary;
