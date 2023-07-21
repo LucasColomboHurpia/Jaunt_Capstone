53b7ee006553128a5008c17c0a1e2b7c7e26806e
Lucas <lucas.c.hurpia@gmail.com>
2023-07-20 18:11:41 -0700

    [style] adds ActivitySurvey styles

--- src/views/app/home/screens/ActivitySurveyResults.js
+++ src/views/app/home/screens/ActivitySurveyResults.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, ActivityIndicator } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -7,8 +7,11 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { TouchableOpacity } from 'react-native';
 import SurveyContext from '../../../../context/SurveyContext';
 
+import { SettingsIcon, PizzaBlack, PizzaWhite } from '../../../../assets/icons/Icon'
+
+
 ///-------------------------------------------------------------------------
-const API_KEY = ""
+const API_KEY = "" //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
 /////-----------------------------------------------------------------------
 
 
@@ -43,7 +46,7 @@ const exampleAPIresponse = `[{"address":"100 W Hastings St, Vancouver, BC V6B 1G
 
 const systemMessage = {
   "role": "system",
-  "content": "You are a helpful assistant that suggests activities and real places to go in Vancouver based on the given parameters. you are not allowed to show places tha are not real business or. if the 'UserWouldLikeTo' is 'Do Something', if the 'UserWouldLikeTo' is 'Eat Something', suggest a real place to eat. The suggestions must always be given in a specific JavaScript array of objects format. For each suggestion, use this format(never use these example values, always use real place values, do not invent places, prioritize business): '[{address:' add the address here', coordinates: {lat:123,lng:123}, name: 'name of place', description: 'briefly describe the place, a short phrase, use emojis at the end',Tips:['suggestion of things to do around the area!','walk around and take a picture of this thing!','example'] ,tags:['example','expensive','healthy']},]'. The 'tags' should reflect the features of the location such as 'healthy', 'expensive', 'popular', etc. The tips should always be recomendations around the main area but never related to the main location, always at least 3 tips. The suggestions should consider the user's preferences, dietary restrictions, and likelihood of enjoying certain food types. The suggestions should be based on the user's 'UserWouldLikeTo', 'Preferences', 'Diet', 'DoNotLike', 'DoLike', and 'ChanceOfEnjoyingMeat' inputs. Always return the suggestions as JavaScript array of objects, not as plain text, do not add any more words in the explanation besides the array. keep your replies brief. generate at least 3 suggestions in one single object based on these parameters. you are not allowed to show results with adress equals to '123 Main St' or '456 Granville St'. The first characters of the message must be '[' and the last ']'"
+  "content": `You are a helpful assistant that suggests activities and real places to go in Vancouver based on the given parameters. you are not allowed to show places tha are not real business. if the 'UserWouldLikeTo' is 'Do Something', if the 'UserWouldLikeTo' is 'Eat Something', suggest a real place to eat. The suggestions must always be given in a specific JavaScript array of objects format. For each suggestion, use this format(never use these example values, always use real place values, do not invent places, prioritize business): '[{address:' add the address here', coordinates: {lat:123,lng:123}, name: 'name of place', description: 'briefly describe the place, a short phrase, use emojis at the end',Tips:['suggestion of things to do around the area!','walk around and take a picture of this thing!','example'] ,tags:['example','expensive','healthy'], matchIcon:'Category'},]'. The 'tags' should reflect the features of the location such as 'healthy', 'expensive', 'popular', etc. The tips should always be recomendations around the main area but never related to the main location, always at least 3 tips. The suggestions should consider the user's preferences, dietary restrictions, and likelihood of enjoying certain food types. The suggestions should be based on the user's 'UserWouldLikeTo', 'Preferences', 'Diet', 'DoNotLike', 'DoLike', and 'ChanceOfEnjoyingMeat' inputs. Always return the suggestions as JavaScript array of objects, not as plain text, do not add any more words in the explanation besides the array. if UserWouldLikeTo is "eat something", the "matchIcon" field must be one of these categories, matching as much as possible to the suggestion: "Steakhouse, Sushi, Italian, Picnic, Seafood, Fast Foot, Pizza, Oriental, Mexican, Vegetarian", and if it is "DoSomething" match the suggestion with one of these: "Bowling, MovieTheater, Beach, Park, Hiking, Bightclub, Bar, Museum, Gallery, AmusementPark, Karaoke, Arcade, Bouldering". keep your replies brief. generate at least 3 suggestions in one single object based on these parameters. The first characters of the message must be '[' and the last ']'`
 };
 const SurveyResults = ({ route, navigation }) => {
   const { surveyData, setSurveyData } = useContext(SurveyContext);
@@ -53,7 +56,19 @@ const SurveyResults = ({ route, navigation }) => {
   const [data, setData] = useState([]);
   const [retryCount, setRetryCount] = useState(0);
 
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [selectedItemForTags, setSelectedItemForTags] = useState(null);
+
+
+  const handleRetry = () => {
+    setIsLoading(true);
+    makeApiCall();
+  };
+
+
   const makeApiCall = async () => {
+
+    setSelectedItem(null);
 
     console.log(`========================================`)
     console.log(`parameters are `, surveyData)
@@ -73,7 +88,7 @@ const SurveyResults = ({ route, navigation }) => {
       setTimeout(() => {
         setData(JSON.parse(exampleAPIresponse));
         setIsLoading(false);
-      }, 7000);
+      }, 700);
     } else {
       fetch("https://api.openai.com/v1/chat/completions", {
         method: "POST",
@@ -94,17 +109,24 @@ const SurveyResults = ({ route, navigation }) => {
             data.choices[0].message.content.includes("456") ||
             data.choices[0].message.content.includes("example")
           ) {
-            throw new Error("Content includes disallowed string");  // Throw an error to trigger the retry
+            // Retry the API call
+            if (retryCount < 2) {
+              setRetryCount(retryCount + 1);
+              makeApiCall();
+            } else {
+              setData([]);
+              setIsLoading(false);
+            }
+          } else {
+            setData(JSON.parse(data.choices[0].message.content));
+            setIsLoading(false);
           }
-
-          setData(JSON.parse(data.choices[0].message.content));
-          setIsLoading(false);
         })
         .catch(error => {
           console.error('Error on API call:', error);
-          // On catch, regardless of error type, increase retryCount to 4 and retry
+          // On catch, regardless of error type, increase retryCount and retry
           if (retryCount < 2) {
-            setRetryCount(4);
+            setRetryCount(retryCount + 1);
             makeApiCall();
           } else {
             setData([]);
@@ -118,22 +140,24 @@ const SurveyResults = ({ route, navigation }) => {
     makeApiCall();
   }, []);
 
-  const handleOptionClick = (item) => {
-    const updatedSurveyData = {
-      ...surveyData,
-      activityParameters: surveyData.activityParameters.map(param => {
-        if (param.id === activityId) {
-          return { ...param, apiResponse: item };
-        }
-        return param;
-      })
-    };
-
-    setSurveyData(updatedSurveyData);
-
-    console.log(surveyData)
-    navigation.navigate('ActivityDashboard', { item, activityId });
+  const handleOptionClick = (item, index) => {
+    if (selectedItem !== index) {
+      setSelectedItem(index);
+    } else {
+      const updatedSurveyData = {
+        ...surveyData,
+        activityParameters: surveyData.activityParameters.map(param => {
+          if (param.id === activityId) {
+            return { ...param, apiResponse: item };
+          }
+          return param;
+        })
+      };
+      setSurveyData(updatedSurveyData);
+      navigation.navigate('ActivityDashboard', { item, activityId });
+    }
   };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Everyone has spoken!</Text>
@@ -142,22 +166,48 @@ const SurveyResults = ({ route, navigation }) => {
       {isLoading ? (
         <ActivityIndicator size="large" color="#0000ff" />
       ) : (
-        data.map((item, index) => (
-          <TouchableOpacity
-            key={index}
-            onPress={() => handleOptionClick(item)}
-          >
-            <View style={styles.optionBox}>
-              <Text style={styles.itemName}>{item.name}</Text>
-              <Text style={styles.itemText}>{item.address}</Text>
-              <Text style={styles.itemDescription}>{item.description}</Text>
-              <Text style={styles.itemTags}>Tags: {item.tags.join(', ')}</Text>
-            </View>
-          </TouchableOpacity>
-        ))
-      )}
+        <ScrollView
+          style={styles.scrollView}
+          contentContainerStyle={styles.contentContainer} 
+        >
+          <View style={styles.contentWrapper}>
+            {data.map((item, index) => (
+              <TouchableOpacity
+                key={index}
+                onPress={() => handleOptionClick(item, index)}
+              >
+                <View style={[
+                  styles.optionBox,
+                  selectedItem === index && styles.selectedOption
+                ]}>
+                  <View style={styles.iconWrapper}>
+                    <View style={styles.iconContainer}>
+                      <PizzaBlack size={70} />
+                    </View>
+                  </View>
+                  <Text style={styles.itemName}>{item.name}</Text>
+                  <Text style={styles.itemText}>{item.address}</Text>
+                  {selectedItem === index && (
+                    <>
+                      <Text style={styles.itemDescription}>{item.description}</Text>
+                      <Text style={styles.itemTags}>Tags: {item.tags.join(', ')}</Text>
+                    </>
+                  )}
+                </View>
+              </TouchableOpacity>
 
-      <Text style={styles.boldText}>Choose wisely</Text>
+            ))}
+            {!isLoading && data.length > 0 && (
+              <TouchableOpacity
+                style={styles.retryButton}
+                onPress={handleRetry}
+              >
+                <Text style={styles.buttonText}>I want to see new options</Text>
+              </TouchableOpacity>
+            )}
+          </View>
+        </ScrollView>
+      )}
     </View>
   );
 };
@@ -168,7 +218,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     padding: 12,
+    backgroundColor: 'white'
   },
+
+  scrollView: {
+    width: '100%',
+    paddingBottom: 10,
+    flex: 1,
+  },
+  contentWrapper: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  contentContainer: {
+    flexGrow: 1,
+    justifyContent: 'center',
+  },
+
   title: {
     fontSize: 24,
     fontWeight: 'bold',
@@ -183,27 +250,50 @@ const styles = StyleSheet.create({
     fontSize: 18,
   },
   optionBox: {
-    backgroundColor: '#cccccc',
+    backgroundColor: '#F3F3F3',
     borderRadius: 10,
     height: 'auto',
     width: '80%',
     marginVertical: 10,
     padding: 10,
   },
+  selectedOption: { // Add this style
+    borderColor: '#F35F4B',
+    borderWidth: 1,
+  },
+
+  iconWrapper: {
+    alignItems: 'center',
+  },
+
+  iconContainer: {
+    width: 70,
+    height: 70,
+    borderRadius: 25,
+    backgroundColor: '#F3F3F3',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 10,
+    marginBottom: 10,
+  },
+
   itemName: {
     fontSize: 20,
     fontWeight: 'bold',
+    textAlign: 'center',
   },
   itemText: {
     fontSize: 16,
+    fontWeight: 'bold',
     color: '#333',
     marginTop: 5,
+    textAlign: 'center',
   },
   itemDescription: {
     fontSize: 18,
     color: '#666',
-    marginTop: 5,
-    marginBottom: 5,
+    marginTop: 7,
+    marginBottom: 10,
   },
   itemTags: {
     fontSize: 12,
@@ -212,6 +302,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#eeeeee',
     borderRadius: 10,
     padding: 5,
+  },
+
+  retryButton: {
+    backgroundColor: '#F35F4B',
+    borderRadius: 15,
+    padding: 10,
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+    width: '100%',
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
   },
 });
 
