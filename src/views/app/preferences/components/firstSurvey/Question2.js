a46f24737b71928c2a59d7acf35b0f067af84f36
Naifar_Nismath <naifarnismath@gmail.com>
2023-07-28 02:28:42 -0700

    addMargin

--- src/views/app/preferences/components/firstSurvey/Question2.js
+++ src/views/app/preferences/components/firstSurvey/Question2.js
@@ -1,14 +1,21 @@
-import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, FlatList, StyleSheet, Button, TextInput } from 'react-native';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  StyleSheet,
+  Button,
+  TextInput,
+} from "react-native";
 
-import { BackIcon, CheckIcon } from '../../../../../assets/icons/Icon'
+import { BackIcon, CheckIcon } from "../../../../../assets/icons/Icon";
 
-
-const answerOptions = ['Shellfish', 'Tree nuts', 'Dairy', 'Glutten', 'Others'];
+const answerOptions = ["Shellfish", "Tree nuts", "Dairy", "Glutten", "Others"];
 
 const Question2 = ({ onAnswer, onGoBack }) => {
   const [selectedOptions, setSelectedOptions] = useState([]);
-  const [customAnswer, setCustomAnswer] = useState('');
+  const [customAnswer, setCustomAnswer] = useState("");
 
   const handleAnswerOptionClick = (option) => {
     setSelectedOptions((prevOptions) => {
@@ -28,25 +35,33 @@ const Question2 = ({ onAnswer, onGoBack }) => {
       <TouchableOpacity
         style={[
           styles.answerOption,
-          { backgroundColor: isSelected ? '#19445A' : '#F3F3F3' },
+          { backgroundColor: isSelected ? "#19445A" : "#F3F3F3" },
         ]}
         onPress={() => handleAnswerOptionClick(item)}
       >
-        {isSelected && <CheckIcon size={20} color='#FFFFFF' />}
-        <Text style={isSelected ? styles.optionTextSelected : styles.optionText}>{item}</Text>
+        {isSelected && <CheckIcon size={20} color="#FFFFFF" />}
+        <Text
+          style={isSelected ? styles.optionTextSelected : styles.optionText}
+        >
+          {item}
+        </Text>
       </TouchableOpacity>
     );
   };
 
-
   const handleSubmit = () => {
-    const finalAnswers = customAnswer ? [...selectedOptions, customAnswer] : selectedOptions;
-    onAnswer(finalAnswers, 'DoNotLike');
-  }
+    const finalAnswers = customAnswer
+      ? [...selectedOptions, customAnswer]
+      : selectedOptions;
+    onAnswer(finalAnswers, "DoNotLike");
+  };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.questionText}>Do you have any medical conditions that restrict you from any extreme activities?</Text>
+      <Text style={styles.questionText}>
+        Do you have any medical conditions that restrict you from any extreme
+        activities?
+      </Text>
 
       <TextInput
         style={styles.input}
@@ -68,31 +83,30 @@ const Question2 = ({ onAnswer, onGoBack }) => {
       <View style={styles.buttonContainer}>
         <TouchableOpacity style={styles.buttonBack} onPress={onGoBack}>
           <View style={styles.backIconContainer}>
-            <BackIcon size={24} color='#000000' />
+            <BackIcon size={24} color="#000000" />
           </View>
         </TouchableOpacity>
         <TouchableOpacity style={styles.buttonNext} onPress={handleSubmit}>
           <Text style={styles.buttonText}>Next Step</Text>
         </TouchableOpacity>
       </View>
-
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     padding: 12,
   },
   questionText: {
-    fontStyle: 'normal',
-    fontWeight: 'normal',
+    fontStyle: "normal",
+    fontWeight: "normal",
     fontSize: 18,
     marginBottom: 20,
     marginTop: 20,
-    textAlign: 'center',
+    textAlign: "center",
   },
   input: {
     height: 60,
@@ -103,68 +117,65 @@ const styles = StyleSheet.create({
     padding: 15,
   },
   answerOptionsContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
 
   optionTextSelected: {
     marginLeft: 9,
-    color: 'white',
+    color: "white",
   },
 
   optionText: {
-    color: 'black',
+    color: "black",
   },
 
-  
   backIconContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   answerOption: {
-    flexDirection: 'row',
+    flexDirection: "row",
     paddingVertical: 20,
     paddingHorizontal: 30,
     fontSize: 20,
     borderRadius: 15,
     margin: 5,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     width: 150,
   },
 
-
-
   buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '100%',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
     paddingHorizontal: 12,
+    marginBottom: 10,
   },
 
   buttonBack: {
     flex: 1,
-    justifyContent: 'flex-start',
+    justifyContent: "flex-start",
   },
 
   buttonNext: {
     flex: 1,
-    justifyContent: 'flex-end',
-    backgroundColor: '#F35F4B',
+    justifyContent: "flex-end",
+    backgroundColor: "#F35F4B",
     borderRadius: 15,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     margin: 10,
     padding: 20,
-    width: '45%',
-    alignItems: 'center'
+    width: "45%",
+    alignItems: "center",
   },
 
   buttonText: {
-    color: 'white',
-    fontWeight: 'bold',
-
-  }
+    color: "white",
+    fontWeight: "bold",
+  },
 });
 
 export default Question2;
